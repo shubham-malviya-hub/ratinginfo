85f5f605de9428f4060b90360e839ace017fd374
Shubham <shubhamm2693@gmail.com>
2020-08-18 00:10:59 +0530

    RATING-INFO-MICROSERVICE-INITIATED

---
+++ src/main/java/com/moviemelas/ratinginfo/RatinginfoApplication.java
@@ -0,0 +1,13 @@
+package com.moviemelas.ratinginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatinginfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatinginfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviemelas/ratinginfo/RatinginfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviemelas.ratinginfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatinginfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
